f4b40580965076c89235976577351296952e33fe
chaitanyakonduru <chaitu.cn@gmail.com>
2013-09-17 00:04:25 +0530

    Test Commit

--- src/com/example/invoiceapp/BaseActivity.java
+++ src/com/example/invoiceapp/BaseActivity.java
@@ -12,6 +12,7 @@ public class BaseActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		prepareActionBar();
+		
 	}
 
 	private void prepareActionBar() {
