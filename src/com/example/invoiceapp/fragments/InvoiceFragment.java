2fdc02bb0d96f974f32de83f6880503065fec404
Chaitanya <chaitanya.konduru@logictreeit.com>
2013-10-03 17:26:33 +0530

    Added Invoice Fragment

--- src/com/example/invoiceapp/fragments/InvoiceFragment.java
+++ src/com/example/invoiceapp/fragments/InvoiceFragment.java
@@ -1,20 +1,26 @@
 package com.example.invoiceapp.fragments;
 
-import android.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import java.util.List;
 
-public class InvoiceFragment extends Fragment {
+import android.app.Activity;
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.example.invoiceapp.CustomerActivity;
+import com.example.invoiceapp.database.DatabaseQueryManager;
+import com.example.invoiceapp.database.DbQueryCallback;
+import com.example.invoiceapp.utils.Constants;
+
+public class InvoiceFragment extends ListFragment implements DbQueryCallback<Object> {
 	
 	
 	private static InvoiceFragment invoiceFragment;
-
-
+	private DatabaseQueryManager databaseQueryManager;
+	private CustomerActivity activity;
+	
 	public InvoiceFragment() {
-		// TODO Auto-generated constructor stub
+		
 	}
 	
 	public static InvoiceFragment newInstance() {
@@ -22,14 +28,49 @@ public class InvoiceFragment extends Fragment {
 		invoiceFragment = new InvoiceFragment();
 		return invoiceFragment;
 	}
-
 	
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		TextView textView=new TextView(getActivity());
-		textView.setText("Invoice");
-		return textView;
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		this.activity=(CustomerActivity) activity;
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		databaseQueryManager=DatabaseQueryManager.getInstance(getActivity());
+		
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		databaseQueryManager.getInvoices(Constants.DB_REQ_FETCH_INVOICES,this.activity.getCustomerId() , this);
+	}
+
+	
+	
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void onQueryCompleted(int requestCode, Object object) {
+		
+		switch (requestCode) {
+		case Constants.DB_REQ_FETCH_INVOICES:
+			if(object!=null && object instanceof List)
+			{
+				List<String> invoiceList=((List<String>) object);
+				if(invoiceList!=null && !invoiceList.isEmpty())
+				{
+					setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, invoiceList));
+				}
+			}
+			
+			break;
+
+		default:
+			break;
+		}
 	}
 
 }
